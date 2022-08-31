020b9e9c9312f9379190d847a0bf36e9bc9fc844
kkihwan22 <jinkh97@gmail.com>
2022-08-31 14:37:54 +0900

    [ first commit ] 프로젝트 생성

---
+++ src/test/java/today/creame/web/ApplicationTests.java
@@ -0,0 +1,13 @@
+package today.creame.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
