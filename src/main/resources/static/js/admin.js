27ae6688366daaf06279d86eb5e370a3980d296b
kkihwan22 <jinkh97@gmail.com>
2022-11-15 21:27:46 +0900

    ( 작업내용 ) 중요정보 제거

---
+++ src/main/resources/static/js/admin.js
@@ -0,0 +1,19 @@
+const tabItem = document.querySelectorAll(".nav_tabs .tabs");
+const tabContent = document.querySelectorAll(".tab_content");
+
+tabItem.forEach((item) => {
+    item.addEventListener("click", tabHandler);
+});
+
+function tabHandler(item) {
+    const tabTarget = item.currentTarget;
+    const target = tabTarget.dataset.tab;
+    tabItem.forEach((title) => {
+        title.classList.remove("active");
+    });
+    tabContent.forEach((target) => {
+        target.classList.remove("target");
+    });
+    document.querySelector("#" + target).classList.add("target");
+    tabTarget.classList.add("active");
+}
