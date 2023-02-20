089c33bc989944a9517d122f1a776ee3fd12669e
kkihwan22 <jinkh97@gmail.com>
2023-02-20 19:09:21 +0900

    [ 버그 ] 인사말 등록 시 500 에러
    - rule 체크 interface가 누락됨

--- src/main/java/today/creame/web/influence/application/model/InfluenceGreetingCreateParameter.java
+++ src/main/java/today/creame/web/influence/application/model/InfluenceGreetingCreateParameter.java
@@ -1,14 +1,17 @@
 package today.creame.web.influence.application.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import today.creame.web.influence.domain.InfluenceGreetingsHistory;
 import today.creame.web.share.aspect.permit.PermitRule;
 import today.creame.web.share.aspect.permit.PermitRuleType;
+import today.creame.web.share.model.BaseParameter;
 
+@NoArgsConstructor
 @Getter
 @ToString
-public class InfluenceGreetingCreateParameter {
+public class InfluenceGreetingCreateParameter implements BaseParameter {
     @PermitRule(type = PermitRuleType.ME) // TODO: 리스트로 변경
     private Long influenceId;
     private Long fileId;
