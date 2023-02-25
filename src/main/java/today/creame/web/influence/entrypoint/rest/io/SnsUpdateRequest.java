c9ee07dcf9ef4d66bc799315260c0c9f7da79bb9
kkihwan22 <jinkh97@gmail.com>
2023-02-26 00:35:21 +0900

    [ 버그 ] SnsRequest 타입 에러

--- src/main/java/today/creame/web/influence/entrypoint/rest/io/SnsUpdateRequest.java
+++ src/main/java/today/creame/web/influence/entrypoint/rest/io/SnsUpdateRequest.java
@@ -1,15 +1,15 @@
 package today.creame.web.influence.entrypoint.rest.io;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.ToString;
 import org.hibernate.validator.constraints.URL;
 import today.creame.web.influence.domain.SnsCompany;
-import today.creame.web.share.entrypoint.validator.EnumConstraint;
 
 @Getter @ToString
 public class SnsUpdateRequest {
-    @EnumConstraint(enumClass = SnsCompany.class)
+    @NotNull
     private SnsCompany snsCompany;
 
     @NotBlank
