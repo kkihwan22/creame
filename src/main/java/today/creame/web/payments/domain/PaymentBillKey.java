89c2c1bc4d3f12c30a8e16001ff9c9df1cb8c460
kkihwan22 <jinkh97@gmail.com>
2023-03-18 21:12:55 +0900

    간편결제 비밀번호 체크 버그 !

--- src/main/java/today/creame/web/payments/domain/PaymentBillKey.java
+++ src/main/java/today/creame/web/payments/domain/PaymentBillKey.java
@@ -13,6 +13,8 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import today.creame.web.m2net.infra.feign.M2netClient;
 import today.creame.web.m2net.infra.feign.io.M2netPaymentRequest;
 import today.creame.web.m2net.infra.feign.io.M2netSimpleResponse;
@@ -29,6 +31,7 @@ import today.creame.web.share.domain.BaseCreatedAndUpdatedDateTime;
 @Getter
 @ToString
 public class PaymentBillKey extends BaseCreatedAndUpdatedDateTime {
+    private final static Logger log = LoggerFactory.getLogger(PaymentBillKey.class);
     private final static double BENEFIT_RATE = 0.01;
     private final static double VAT = 0.1;
 
@@ -82,7 +85,8 @@ public class PaymentBillKey extends BaseCreatedAndUpdatedDateTime {
     }
 
     public void pay(String password, int amount, Member member, M2netClient client) {
-        if (this.creditCard.isMatchedPassword(password)) {
+        if (!this.creditCard.isMatchedPassword(password)) {
+            log.error("credit card password: {}, requested password: {}", this, creditCard.getPaymentPassword(), password);
             throw new NotMatchedPasswordException();
         }
 
