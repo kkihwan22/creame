85a5c2b5a9392fa0561220e24687796501da363a
keehwan <keehwan@weversecompany.com>
2023-05-21 16:38:07 +0900

    feature : 불필요한 쿼리 제거

--- src/main/java/today/creame/web/member/entrypoint/rest/MemberQueryRestController.java
+++ src/main/java/today/creame/web/member/entrypoint/rest/MemberQueryRestController.java
@@ -1,6 +1,5 @@
 package today.creame.web.member.entrypoint.rest;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,13 +8,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import today.creame.web.config.security.CustomUserDetails;
-import today.creame.web.influence.application.model.InfluenceQuestionResult;
 import today.creame.web.member.application.MemberQuery;
 import today.creame.web.member.application.model.MeResult;
-import today.creame.web.member.application.model.MyQuestionsQueryParameter;
 import today.creame.web.member.domain.MemberNotificationPreference;
 import today.creame.web.member.entrypoint.rest.io.MeResponse;
 import today.creame.web.share.entrypoint.BaseRestController;
@@ -29,19 +25,6 @@ import today.creame.web.share.support.SecurityContextSupporter;
 public class MemberQueryRestController implements BaseRestController {
     private final Logger log = LoggerFactory.getLogger(MemberQueryRestController.class);
     private final MemberQuery memberQuery;
-
-    @GetMapping("/api/members/{id}/questions")
-    public ResponseEntity<Body<List<InfluenceQuestionResult>>> pagingListMyQuestions(
-        @PathVariable Long id,
-        @RequestParam(required = false) int page,
-        @RequestParam(required = false) int size,
-        @RequestParam(required = false) boolean answered
-    ) {
-        List<InfluenceQuestionResult> results = memberQuery.pagingListMyQuestions(
-            new MyQuestionsQueryParameter(page, size, answered));
-        log.debug("results: {}", results);
-        return ResponseEntity.ok(BodyFactory.success(results));
-    }
 
     @GetMapping("/api/v1/me")
     public ResponseEntity<Body<MeResponse>> getMe(@AuthenticationPrincipal UserDetails userDetails) {
