f37c3c5bf74ad4c0590418aacd60d12ce5e074d0
keehwan <keehwan@weversecompany.com>
2023-09-02 22:26:23 +0900

    - 테이블 변경에 따른 엔티티 변경

--- src/main/java/today/creame/web/influence/application/model/InfluenceResult.java
+++ src/main/java/today/creame/web/influence/application/model/InfluenceResult.java
@@ -71,7 +71,7 @@ public class InfluenceResult {
         this.reviewAnswerCount = influence.getReviewAnswerCount();
         this.qnaCount = influence.getQnaCount();
         this.answerCount = influence.getQnaAnswerCount();
-        this.item = item.getId();
+        this.item = Optional.ofNullable(item.getId()).orElse(null);
 
         this.coinPrice = item.getPrice();
         this.coinPriceTime = item.getPricePerTime();
