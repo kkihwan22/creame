64536091095b00f73782f997a825280dde83d6f4
keehwan <keehwan@weversecompany.com>
2023-09-10 13:20:39 +0900

    - 최근 인사말 등록 조회 - 인사말 업데이트 기능

--- src/main/java/today/creame/web/matching/entrypoint/rest/SettlementRestController.java
+++ src/main/java/today/creame/web/matching/entrypoint/rest/SettlementRestController.java
@@ -8,9 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import today.creame.web.influence.exception.BadRequestException;
 import today.creame.web.matching.applicaton.MatchingQueryService;
 import today.creame.web.matching.applicaton.model.MatchingStatisticsParameter;
 import today.creame.web.matching.applicaton.model.MatchingStatisticsResult;
+import today.creame.web.matching.domain.PaidType;
 import today.creame.web.matching.entrypoint.rest.io.MatchingStatisticsDetailResponse;
 import today.creame.web.matching.entrypoint.rest.io.MatchingStatisticsResponse;
 import today.creame.web.share.entrypoint.BaseRestController;
@@ -18,10 +20,13 @@ import today.creame.web.share.entrypoint.Body;
 import today.creame.web.share.entrypoint.BodyFactory;
 import today.creame.web.share.support.SecurityContextSupporter;
 
+import java.lang.reflect.Array;
 import java.time.LocalTime;
 import java.util.*;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+import static today.creame.web.m2net.domain.DeductionMethod.POST;
 import static today.creame.web.matching.domain.PaidType.PRE;
 
 @RequiredArgsConstructor
@@ -63,37 +68,16 @@ public class SettlementRestController implements BaseRestController {
 
     @GetMapping("/api/v1/matchings/stat/selected")
     public ResponseEntity<Body<MatchingStatisticsDetailResponse>> getMatchingsStatSelectedDate(@RequestParam String date) {
-        if(Objects.isNull(date)) {
-            return ResponseEntity.ok(BodyFactory.success(null));
+        if (Objects.isNull(date)) {
+            throw new BadRequestException();
         }
+
         Long id = SecurityContextSupporter.getId();
         List<MatchingStatisticsResult> results = matchingQueryService.getConsultationHoursPerMonth(new MatchingStatisticsParameter(id, date));
-
-        Map<String, List<MatchingStatisticsResult>> map = results.stream().collect(Collectors.groupingBy(MatchingStatisticsResult::getYearMonth));
-        Set<String> keys = map.keySet();
-        MatchingStatisticsDetailResponse response = null;
-        for(String key: keys) {
-            List<MatchingStatisticsResult> list = map.get(key);
-            Integer preTime = 0, postTime = 0, totalTime = 0;
-            Long preCoin = 0L, postCoin = 0L, totalCoin = 0L;
-            int totalSeconds = 0;
-
-            for(MatchingStatisticsResult target: list) {
-                if(PRE.equals(target.getPaidType())) {
-                    preTime = target.getTotalTime();
-                    preCoin = target.getTotalCoin();
-                } else {
-                    postTime = target.getTotalTime();
-                    postCoin = target.getTotalCoin();
-                }
-                totalCoin += target.getTotalCoin();
-                totalSeconds += target.getTotalTime();
-            }
-
-            totalTime = totalSeconds;
-            response = new MatchingStatisticsDetailResponse(key, postTime, postCoin, preTime, preCoin, totalTime, totalCoin);
-        }
-
+        Map<String, List<MatchingStatisticsResult>> resultMap = results.stream().collect(Collectors.groupingBy(MatchingStatisticsResult::getYearMonth));
+        List<MatchingStatisticsResult> resultFlatMap = resultMap.values().stream().flatMap(List::stream).collect(Collectors.toList());
+        Map<PaidType, List<MatchingStatisticsResult>> groupByPaidTypeMap = resultFlatMap.stream().collect(Collectors.groupingBy(MatchingStatisticsResult::getPaidType));
+        MatchingStatisticsDetailResponse response = new MatchingStatisticsDetailResponse(date, groupByPaidTypeMap.getOrDefault(PRE, List.of()), groupByPaidTypeMap.getOrDefault(POST, List.of()));
         return ResponseEntity.ok(BodyFactory.success(response));
     }
 
--- src/main/java/today/creame/web/matching/entrypoint/rest/io/MatchingStatisticsDetailResponse.java
+++ src/main/java/today/creame/web/matching/entrypoint/rest/io/MatchingStatisticsDetailResponse.java
@@ -1,26 +1,38 @@
 package today.creame.web.matching.entrypoint.rest.io;
 
 import lombok.Getter;
+import today.creame.web.matching.applicaton.model.MatchingStatisticsResult;
 
 import java.time.LocalTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Getter
 public class MatchingStatisticsDetailResponse {
     private String yearMonth;
-    private Integer postTime;
-    private Long postCoin;
-    private Integer preTime;
-    private Long preCoin;
-    private Integer totalTime;
-    private Long totalCoin;
+    private int postTime;
+    private long postCoin;
+    private int preTime;
+    private long preCoin;
+    private int totalTime;
+    private long totalCoin;
 
-    public MatchingStatisticsDetailResponse(String yearMonth, Integer postTime, Long postCoin, Integer preTime, Long preCoin, Integer totalTime, Long totalCoin) {
+    public MatchingStatisticsDetailResponse(String yearMonth, List<MatchingStatisticsResult> preResults, List<MatchingStatisticsResult> postResults) {
         this.yearMonth = yearMonth;
-        this.postTime = postTime;
-        this.postCoin = postCoin;
-        this.preTime = preTime;
-        this.preCoin = preCoin;
-        this.totalTime = totalTime;
-        this.totalCoin = totalCoin;
+        if (!preResults.isEmpty()) {
+            MatchingStatisticsResult preResult = preResults.get(0);
+            this.preCoin = preResult.getTotalCoin();
+            this.preTime = preResult.getTotalTime();
+        }
+
+        if (!postResults.isEmpty()) {
+            MatchingStatisticsResult postResult = postResults.get(0);
+            this.postCoin = postResult.getTotalCoin();
+            this.postTime = postResult.getTotalTime();
+        }
+
+        this.totalCoin = postCoin + preCoin;
+        this.totalTime = postTime + preTime;
     }
 }
