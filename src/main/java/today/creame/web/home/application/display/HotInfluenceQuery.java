d5ca987332663f54df5d385aab6ffb9d1a7cebf3
keehwan <keehwan@weversecompany.com>
2023-11-08 23:58:53 +0900

    - 급상승 정렬 변경

--- src/main/java/today/creame/web/home/application/display/HotInfluenceQuery.java
+++ src/main/java/today/creame/web/home/application/display/HotInfluenceQuery.java
@@ -1,9 +1,6 @@
 package today.creame.web.home.application.display;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
@@ -13,20 +10,17 @@ import today.creame.web.home.entrypoint.io.HomeQueryParam;
 import today.creame.web.influence.application.InfluenceQuery;
 import today.creame.web.influence.application.model.InfluenceResult;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class HotInfluenceQuery implements DisplayQuery {
-    private final Logger log = LoggerFactory.getLogger(HotInfluenceQuery.class);
     private final InfluenceQuery influenceQuery;
 
     @Override
     public List<InfluenceResult> list(HomeQueryParam parameter) {
-        PageRequest pageRequest = parameter.getPageRequest();
-        pageRequest.withSort(Sort.by(
-            Order.desc("reviewCount"),
-            Order.desc("createdDateTime")
-        ));
-
+        Sort orders = Sort.by(Order.desc("createdDateTime"), Order.desc("createdDateTime"));
+        PageRequest pageRequest = parameter.getPageRequest().withSort(orders);
         return influenceQuery.listAll(pageRequest);
     }
 }
